e09094a61b791f415be57b655e32d34e6995eff2
Ashutosh-dotcom <kumar.ashutosh81091@gmail.com>
2023-08-03 16:27:45 +0530

    capp

---
+++ app/src/main/java/com/example/coroutineapp2/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.coroutineapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.coroutineapp2.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        SayHelloFromMainThread()
+        SayHelloFromBackThread()
+    }
+    private fun SayHelloFromMainThread() {
+        CoroutineScope(Dispatchers.Main).launch {
+            binding.text1.text = "Hello From" +
+                    "${Thread.currentThread().name}"
+        }
+
+    }
+
+    private fun SayHelloFromBackThread() {
+        TODO("Not yet implemented")
+    }
+
+
+}
